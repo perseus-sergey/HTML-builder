0c084e3b5e939cb967f43385bd71f8a2521bb5a7
perseus <sergeygubriy@gmail.com>
2023-05-04 04:15:02 +0300

    docs: resolve 02-write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+const { stdout } = process;
+
 const pathToTxt = path.join(__dirname, 'text.txt');
 
 let readStream = new fs.createReadStream(pathToTxt, 'utf-8');
 
-readStream.on('data', (chunk) => console.log(chunk));
+// readStream.on('data', (chunk) => console.log(chunk));
+readStream.on('data', (chunk) => stdout.write(chunk + '\n'));
+
+// fs.readFile(pathToTxt, 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('node:readline');
+const {
+  stdin: input,
+  stdout: output,
+} = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+const pathToTxt = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(pathToTxt);
+const exitDialog = () => {
+  console.log('Goodby!');
+  process.exit();
+};
+
+console.log('Hello! Input something:');
+
+rl.on('line', (input) => {
+  const strInput = input.toString();
+  if (strInput === 'exit') exitDialog();
+
+  console.log('Do write, don\'t stop (or input \'exit\')');
+
+  writeStream.write(`${strInput}\n`);
+});
+
+rl.on('SIGINT', () => {
+  exitDialog();
+});
